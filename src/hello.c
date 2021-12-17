86a6ef0adf401548b62be33ee0fde4941dd522c5
YUPENG123 <yupengdongguan@hotmail.com>
2021-12-18 00:45:55 +0800

    123

--- src/hello.c
+++ src/hello.c
@@ -1,11 +1,11 @@
 #include "../inc/hello.h" 
-//#include "../inc/add.h" 
+#include "../inc/add.h" 
 int a=10;
 int b=20;
 int main(int argc,char *argv[])
 {
 	printf("Hello world!!\n");
-    //printf("a+b=%d\n",add(a,b));
+    printf("a+b=%d\n",add(a,b));
     printf("Hello world!!\n");
     return (0);
 }
